5017dc64b9220f43935f58f6ffa52ea54880dbc1
Gokul1995 <gokulbudha@gmail.com>
2023-08-02 12:13:45 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import MyButton from './MyButton';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello there!</h1>
+      <p>Let us know about your queries :)</p>
+
+      <MyButton />
+      <MyButton />
+      <MyButton />
+    </>
+  );
+}
